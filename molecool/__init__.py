58f094f1c9346b57b91fb8a57d8f59fd6f9d04ef
Jonathan Moussa <jemoussa@vt.edu>
2022-01-25 02:13:04 -0500

    modifications from exercises

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,11 @@
 """educational reference project"""
 
 # Add imports here
-from .molecool import *
+from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list, calculate_molecular_mass, calculate_center_of_mass
+from .visualize import draw_molecule, bond_histogram
+from .io import open_pdb, open_xyz, write_xyz
 
 # Handle versioneer
 from ._version import get_versions
